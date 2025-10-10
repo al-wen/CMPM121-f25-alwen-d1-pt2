3726d82342464a4304bb371b6f19f116b2d55dca
eating <107654570+al-wen@users.noreply.github.com>
2025-10-10 08:05:40 +0000

    Clicking increases a counter (step 2 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 //import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter: number = 0;
+
 // Create basic HTML structure
 document.body.innerHTML = `
   <h1>D1</h1>
-  
+  <p>Clowns: <span id="counter">0</span></p>
   <button id="increment">🤡</button>
 `;
+
+// Add click handler
+const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
+
+button.addEventListener("click", () => {
+  console.log("Clowns:", button, counterElement, counter);
+  counter++;
+  counterElement.textContent = counter.toString();
+});
